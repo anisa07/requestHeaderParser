c1329dab119ab3881e7093b30b475cec5cf290c1
Anisa <askany07@gmail.com>
2016-06-07 18:56:17 +0000

    project is updated

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,67 @@
 'use strict';
-
+//var os = require('os');
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+	// function isLoggedIn (req, res, next) {
+	// 	if (req.isAuthenticated()) {
+	// 		return next();
+	// 	} else {
+	// 		res.redirect('/login');
+	// 	}
+	// }
 
-	var clickHandler = new ClickHandler();
+	// var clickHandler = new ClickHandler();
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
 		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+			//res.sendFile(path + '/public/index.html');
+			//console.log(os.platform()); 
+			var info = req.headers['user-agent'];
+			if (info.indexOf("Windows") > -1){
+				var os = info.slice(info.indexOf("Windows"), info.indexOf(")")); 	
+			}
+			if (info.indexOf("Mac OS") > -1){
+				var os = info.slice(info.indexOf("Mac OS"), info.indexOf(")")); 	
+			}
+			var ip = req.ip.slice(req.ip.lastIndexOf(":")+1);
+			res.json({ ip: ip, language: req.acceptsLanguages(), system: os});
 		});
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+	// app.route('/login')
+	// 	.get(function (req, res) {
+	// 		res.sendFile(path + '/public/login.html');
+	// 	});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+	// app.route('/logout')
+	// 	.get(function (req, res) {
+	// 		req.logout();
+	// 		res.redirect('/login');
+	// 	});
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+	// app.route('/profile')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.sendFile(path + '/public/profile.html');
+	// 	});
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+	// app.route('/api/:id')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.json(req.user.github);
+	// 	});
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
+	// app.route('/auth/github')
+	// 	.get(passport.authenticate('github'));
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	// app.route('/auth/github/callback')
+	// 	.get(passport.authenticate('github', {
+	// 		successRedirect: '/',
+	// 		failureRedirect: '/login'
+	// 	}));
+
+	// app.route('/api/:id/clicks')
+	// 	.get(isLoggedIn, clickHandler.getClicks)
+	// 	.post(isLoggedIn, clickHandler.addClick)
+	// 	.delete(isLoggedIn, clickHandler.resetClicks);
 };
